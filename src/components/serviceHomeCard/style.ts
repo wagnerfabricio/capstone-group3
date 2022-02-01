36823ca608be3221cc72d838397fbb945c9c92e3
luizpcardoso <luizpauloreiscardosoo@gmail.com>
2022-01-31 22:09:34 -0300

    feat/ home page updated, v1.1

--- src/components/serviceHomeCard/style.ts
+++ src/components/serviceHomeCard/style.ts
@@ -1,31 +1,41 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 300px;
-  height: 350px;
-  background-color: #cb4254;
+  width: 20%;
+  max-width: 300px;
+  height: 50%;
+  max-height: 350px;
+  background-color: #a12c67;
   margin: 0;
   color: #ffffff;
   border-radius: 15px;
+  overflow: hidden;
 
   .image {
-    height: 60%;
+    height: 55%;
     width: 100%;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
     img {
       width: 100%;
+      height: auto;
       margin: 0;
-      border-radius: 15px 15px 0 0;
     }
   }
 
   .details {
     padding: 15px;
+
     h3 {
-      font-size: 30px;
+      font-size: 25px;
       margin-bottom: 10px;
+      font-family: Arial, Helvetica, sans-serif;
     }
     p {
+      font-family: Arial, Helvetica, sans-serif;
     }
   }
 `;
